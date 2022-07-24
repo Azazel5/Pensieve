29e3d993e999f57d2884eefadc08a0263d153552
Azazel5 <subhanga2013@gmail.com>
2022-07-24 12:02:35 -0400

    Added react-media-recorder
    - Start and stop recording audio and display it in a video tag
    - Now, effectively save the audio blob to a backend + think about how to
    display the audio controls i.e. the interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,33 @@
+// React
+import { useState } from 'react'
+
 // 3rd party
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
+import { useReactMediaRecorder } from "react-media-recorder";
 
 const App = () => {
-  return (
-    <div>
-        <Calendar minDate={new Date()} />
-    </div>
-  );
+    const [clickedDay, setClickedDay] = useState()
+    const { status, startRecording, stopRecording, mediaBlobUrl } =
+        useReactMediaRecorder({ video: false });
+
+    return (
+        <div>
+            <Calendar minDate={new Date()} onClickDay={(e) => setClickedDay(e)} />
+
+            <div>
+                <button disabled={!clickedDay || status === 'recording'} onClick={startRecording}>
+                    Start Recording
+                </button>
+
+                <button disabled={!clickedDay || status === 'stopped'} onClick={stopRecording}>
+                    End Recording
+                </button>
+            </div>
+
+            {mediaBlobUrl && <video src={mediaBlobUrl} controls />}
+        </div>
+    );
 }
 
 export default App;
